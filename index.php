6f5850f2539a17962815ccb937b60e7d7b4100f6
Nathan Girot <znathangirotz@gmail.com>
2018-12-09 12:11:02 +0100

    conf supprimé + ajout de conf.sample (fichier qui est utile seulement pour les nvx arrivants)

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+require_once('config/Conf.php');
 
-	require_once('lib/File.php');
-	require_once(File::build_path(array('controller', 'routeur.php')));
+// When debug is enabled display error message and stacktrace
+if (Conf::getDebug()) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
-?>
+require_once('lib/File.php');
+require_once(File::build_path(array('controller', 'routeur.php')));
