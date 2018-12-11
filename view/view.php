89013075d75e48fb0516a465fba1a03566128bc6
Nathan Girot <znathangirotz@gmail.com>
2018-12-11 16:21:33 +0100

    images on off

--- view/view.php
+++ view/view.php
@@ -40,11 +40,11 @@
 					else echo '<div><a href="?controller=Clients&action=Contact">Contact</a></div>';
 
 				?>
-				<div><?php if(isset($_SESSION['pseudo'])) echo '<a href="?controller=Clients&action=Compte"> <img class = "compte" src = images/compte.png > </a>'; else echo '<a href="?controller=Clients&action=Connexion">Se connecter</a>'; ?></div>
+				<div><?php if(isset($_SESSION['pseudo'])) echo '<a href="?controller=Clients&action=Compte"> <img class = "compte" src = images/compte.png > </a>'; else echo '<a href="?controller=Clients&action=Connexion"><img class = "onoff" src = images/on.png ></a>'; ?></div>
 				<?php 
 
 					if(isset($_SESSION['pseudo']))
-						echo '<div><a href="?controller=Clients&action=Deconnexion">Deconnexion</a></div>';
+						echo '<div><a href="?controller=Clients&action=Deconnexion"><img class = "onoff" src = images/off.png ></a></div>';
 
 				?>
 			</nav>
